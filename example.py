c21ceec95cbc6fc8ec631c94baf7d8a167d52b56
SeaTurtle22 <joey@grayturtle.co>
2023-02-27 10:16:50 -0700

    example.py coppied and committed

---
+++ example.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return a+b
+
+def subtract(a,b):
+    return a+b
+
+def test_add():
+    assert add(2,3)==5
+    assert add("space"+"ship")=="spaceship"
+
+#def test_subtract():
+#   assert subtract(2,3)==-1
